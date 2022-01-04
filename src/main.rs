17bc027a8817aa2270aa000fb107a32955b62f51
Josh Kasuboski <josh.kasuboski@gmail.com>
2022-01-03 20:14:52 -0600

    retrieving hn rss feed

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use futures::lock::Mutex;
 use rweb::*;
 use askama_warp::Template;
 use serde::{Serialize, Deserialize};
-use url::{Url};
 use chrono::{DateTime, Utc};
+use feed_rs::parser;
 
 #[derive(Schema, Deserialize, Serialize)]
 struct Healthz {
@@ -27,22 +27,37 @@ struct Dump {
 #[derive(Clone, Schema, Deserialize, Serialize)]
 struct Feed {
     name: String,
-    url: Url,
-    favicon: Url,
+    site_url: String,
+    feed_url: String,
+    favicon: String,
     last_fetched: DateTime<Utc>,
     fetch_error: Option<String>,
     category: String,
 }
 
-#[derive(Clone, Schema, Deserialize, Serialize)]
+#[derive(Default, Clone, Schema, Deserialize, Serialize)]
 struct Entry {
     title: String,
-    content_link: Url,
-    comments_link: Url,
-    robust_link: Url,
+    content_link: String,
+    comments_link: Option<String>,
+    robust_link: String,
     read: bool,
     starred: bool,
     feed: String,
+}
+
+impl From<feed_rs::model::Entry> for Entry {
+    fn from(e: feed_rs::model::Entry) -> Self {
+        let content_link = e.links.into_iter().nth(0).map(|l| l.href.to_string()).unwrap_or("".to_string()).clone();
+        Entry {
+            title: default_text(e.title),
+            content_link: content_link,
+            comments_link: None,
+            read: false,
+            starred: false,
+            ..Default::default()
+        }
+    }
 }
 
 #[derive(Template)]
@@ -74,24 +89,36 @@ async fn main() {
         ])
         .allow_methods(vec!["GET", "HEAD", "POST", "DELETE"]);
 
-    let entries = vec![Entry {
-        title: "I'm cool".to_string(),
-        content_link: Url::parse("https://hackernews.com/post/cool").unwrap(),
-        comments_link: Url::parse("https://hackernews.com/post/cool/comments").unwrap(),
-        robust_link: Url::parse("https://archive.li/xwe8s").unwrap(),
-        read: false,
-        starred: false,
-        feed: "HackerNews".to_string(),
-    }];
-
     let feeds = vec![Feed {
         name: "HackerNews".to_string(),
-        url: Url::parse("https://hackernews.com").unwrap(),
-        favicon: Url::parse("https://hackernews.com/favicon").unwrap(),
+        site_url: "https://news.ycombinator.com".to_string(),
+        feed_url: "https://news.ycombinator.com/rss".to_string(),
+        favicon: "https://hackernews.com/favicon".to_string(),
         last_fetched: Utc::now(),
         fetch_error: None,
         category: "tech".to_string(),
     }];
+
+    let hn = &feeds[0];
+    let hn_resp = reqwest::get(hn.feed_url.to_string())
+        .await
+        .expect("couldn't get feed")
+        .bytes()
+        .await
+        .expect("couldn't pull bytes");
+
+    let hn_feed = match parser::parse_with_uri(hn_resp.as_ref(), Some(&hn.feed_url.to_string())) {
+        Ok(feed) => feed,
+        Err(_error) => panic!(),
+    };
+
+    let entries: Vec<Entry> = hn_feed.entries.into_iter()
+        .map(|e| {
+            let mut o: Entry = e.into();
+            o.feed = hn.site_url.to_string();
+            o
+        })
+        .collect();
 
     let db: DB = Default::default();
     {
@@ -133,4 +160,8 @@ async fn dump(#[data] db: DB) -> Result<Json<Dump>, Rejection> {
         feeds,
         entries,
     }.into())
+}
+
+fn default_text(text: Option<feed_rs::model::Text>) -> String {
+    text.and_then(|t| Some(t.content)).unwrap_or("".to_string())
 }
