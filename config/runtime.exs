c0686bf8a963f43fb0c7b4e52fdfbdcaf3ed930c
Josh Kasuboski <josh.kasuboski@gmail.com>
2026-04-06 22:30:09 +0100

    Make public URL scheme/port configurable via env vars

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,11 +48,13 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PHX_PUBLIC_PORT") || "80")
+  scheme = System.get_env("PHX_SCHEME") || "http"
 
   config :feedreader, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :feedreader, FeedreaderWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port, scheme: scheme],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
