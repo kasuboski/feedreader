05237519aaae8864cdf9d3610b8dec6ad17e7a94
Josh Kasuboski <josh.kasuboski@gmail.com>
2026-04-06 22:35:27 +0100

    Remove force_ssl - proxy handles TLS termination

--- config/prod.exs
+++ config/prod.exs
@@ -8,18 +8,6 @@ import Config
 config :feedreader, FeedreaderWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Force using SSL in production. This also sets the "strict-security-transport" header,
-# known as HSTS. If you have a health check endpoint, you may want to exclude it below.
-# Note `:force_ssl` is required to be set at compile-time.
-config :feedreader, FeedreaderWeb.Endpoint,
-  force_ssl: [
-    rewrite_on: [:x_forwarded_proto],
-    exclude: [
-      # paths: ["/health"],
-      hosts: ["localhost", "127.0.0.1"]
-    ]
-  ]
-
 # Configure Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
 
